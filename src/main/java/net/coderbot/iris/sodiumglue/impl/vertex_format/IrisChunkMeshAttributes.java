ac4a16ed4b9491101e96f211e8bfdefcccb40b3d
coderbot <coderbot16@gmail.com>
2021-11-16 00:06:05 -0800

    Replace ChunkMeshAttribute patch with enum extension mixins

--- src/main/java/net/coderbot/iris/sodiumglue/impl/vertex_format/IrisChunkMeshAttributes.java
+++ src/main/java/net/coderbot/iris/sodiumglue/impl/vertex_format/IrisChunkMeshAttributes.java
@@ -2,9 +2,12 @@ package net.coderbot.iris.sodiumglue.impl.vertex_format;
 
 import me.jellysquid.mods.sodium.client.render.chunk.format.ChunkMeshAttribute;
 
+/**
+ * Initialized by {@link net.coderbot.iris.sodiumglue.mixin.vertex_format.MixinChunkMeshAttribute}
+ */
 public class IrisChunkMeshAttributes {
-    public static ChunkMeshAttribute NORMAL = ChunkMeshAttribute.NORMAL;
-    public static ChunkMeshAttribute TANGENT = ChunkMeshAttribute.TANGENT;
-    public static ChunkMeshAttribute MID_TEX_COORD = ChunkMeshAttribute.MID_TEX_COORD;
-    public static ChunkMeshAttribute BLOCK_ID = ChunkMeshAttribute.BLOCK_ID;
+    public static ChunkMeshAttribute NORMAL;
+    public static ChunkMeshAttribute TANGENT;
+    public static ChunkMeshAttribute MID_TEX_COORD;
+    public static ChunkMeshAttribute BLOCK_ID;
 }
