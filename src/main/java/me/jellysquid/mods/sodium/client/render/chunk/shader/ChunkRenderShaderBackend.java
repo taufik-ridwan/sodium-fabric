b643743085b7ba9755a6778815b0cf91a134bf8c
coderbot <coderbot16@gmail.com>
2021-11-15 21:47:48 -0800

    Pull large amounts of code into IrisChunkProgramOverrides
    This also significantly reduces the number of chunk programs we compile - now we only compile 3 programs, not 30.

--- src/main/java/me/jellysquid/mods/sodium/client/render/chunk/shader/ChunkRenderShaderBackend.java
+++ src/main/java/me/jellysquid/mods/sodium/client/render/chunk/shader/ChunkRenderShaderBackend.java
@@ -11,26 +11,22 @@ import me.jellysquid.mods.sodium.client.render.chunk.ChunkRenderBackend;
 import me.jellysquid.mods.sodium.client.render.chunk.format.ChunkMeshAttribute;
 import me.jellysquid.mods.sodium.client.render.chunk.passes.BlockRenderPass;
 
-import net.coderbot.iris.Iris;
-import net.coderbot.iris.gl.program.ProgramSamplers;
+import net.coderbot.iris.sodiumglue.backend.IrisChunkProgramOverrides;
 import net.coderbot.iris.gl.program.ProgramUniforms;
-import net.coderbot.iris.pipeline.SodiumTerrainPipeline;
-import net.coderbot.iris.pipeline.WorldRenderingPipeline;
 import net.coderbot.iris.shadows.ShadowRenderingState;
-import net.coderbot.iris.sodiumglue.IrisChunkShaderBindingPoints;
-import net.coderbot.iris.sodiumglue.backend.IrisChunkProgram;
 import net.coderbot.iris.sodiumglue.duck.ChunkRenderBackendExt;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
 
 import java.util.EnumMap;
-import java.util.Locale;
-import java.util.Optional;
 
 public abstract class ChunkRenderShaderBackend<T extends ChunkGraphicsState>
         implements ChunkRenderBackend<T>, ChunkRenderBackendExt {
-    private final EnumMap<ChunkFogMode, EnumMap<BlockRenderPass, ChunkProgram>> programs = new EnumMap<>(ChunkFogMode.class);
-    private final EnumMap<ChunkFogMode, EnumMap<BlockRenderPass, ChunkProgram>> shadowPrograms = new EnumMap<>(ChunkFogMode.class);
+    // Iris start
+    private final IrisChunkProgramOverrides irisChunkProgramOverrides = new IrisChunkProgramOverrides();
+    private RenderDevice device;
+    // Iris end
+    private final EnumMap<ChunkFogMode, ChunkProgram> programs = new EnumMap<>(ChunkFogMode.class);
 
     protected final ChunkVertexType vertexType;
     protected final GlVertexFormat<ChunkMeshAttribute> vertexFormat;
@@ -42,141 +38,38 @@ public abstract class ChunkRenderShaderBackend<T extends ChunkGraphicsState>
         this.vertexFormat = vertexType.getCustomVertexFormat();
     }
 
-    private GlShader createVertexShader(RenderDevice device, ChunkFogMode fogMode, BlockRenderPass pass, boolean shadow, SodiumTerrainPipeline pipeline) {
-        if (pipeline != null) {
-            Optional<String> irisVertexShader;
-
-            if (shadow) {
-                irisVertexShader = pipeline.getShadowVertexShaderSource();
-            } else {
-                irisVertexShader = pass.isTranslucent() ? pipeline.getTranslucentVertexShaderSource() : pipeline.getTerrainVertexShaderSource();
-            }
-
-            if (irisVertexShader.isPresent()) {
-                return new GlShader(device, ShaderType.VERTEX, new Identifier("iris", "sodium-terrain.vsh"),
-                        irisVertexShader.get(), ShaderConstants.builder().build());
-            }
-        }
-
-        return ShaderLoader.loadShader(device, ShaderType.VERTEX,
+    private ChunkProgram createShader(RenderDevice device, ChunkFogMode fogMode, GlVertexFormat<ChunkMeshAttribute> vertexFormat) {
+        GlShader vertShader = ShaderLoader.loadShader(device, ShaderType.VERTEX,
                 new Identifier("sodium", "chunk_gl20.v.glsl"), fogMode.getDefines());
-    }
 
-    private GlShader createGeometryShader(RenderDevice device, ChunkFogMode fogMode, BlockRenderPass pass, boolean shadow, SodiumTerrainPipeline pipeline) {
-        if (pipeline != null) {
-            Optional<String> irisGeometryShader;
-
-            if (shadow) {
-                irisGeometryShader = pipeline.getShadowGeometryShaderSource();
-            } else {
-                irisGeometryShader = pass.isTranslucent() ? pipeline.getTranslucentGeometryShaderSource() : pipeline.getTerrainGeometryShaderSource();
-            }
-
-            if (irisGeometryShader.isPresent()) {
-                return new GlShader(device, ShaderType.GEOMETRY, new Identifier("iris", "sodium-terrain.gsh"),
-                        irisGeometryShader.get(), ShaderConstants.builder().build());
-            }
-        }
-
-        return null;
-    }
-
-    private GlShader createFragmentShader(RenderDevice device, ChunkFogMode fogMode, BlockRenderPass pass, boolean shadow, SodiumTerrainPipeline pipeline) {
-        if (pipeline != null) {
-            Optional<String> irisFragmentShader;
-
-            if (shadow) {
-                irisFragmentShader = pipeline.getShadowFragmentShaderSource();
-            } else {
-                irisFragmentShader = pass.isTranslucent() ? pipeline.getTranslucentFragmentShaderSource() : pipeline.getTerrainFragmentShaderSource();
-            }
-
-            if (irisFragmentShader.isPresent()) {
-                return new GlShader(device, ShaderType.FRAGMENT, new Identifier("iris", "sodium-terrain.fsh"),
-                        irisFragmentShader.get(), ShaderConstants.builder().build());
-            }
-        }
-
-        return ShaderLoader.loadShader(device, ShaderType.FRAGMENT,
+        GlShader fragShader = ShaderLoader.loadShader(device, ShaderType.FRAGMENT,
                 new Identifier("sodium", "chunk_gl20.f.glsl"), fogMode.getDefines());
-    }
-
-    private ChunkProgram createShader(RenderDevice device, ChunkFogMode fogMode, BlockRenderPass pass,
-                                      GlVertexFormat<ChunkMeshAttribute> vertexFormat, boolean shadow,
-                                      SodiumTerrainPipeline pipeline) {
-        GlShader vertShader = createVertexShader(device, fogMode, pass, shadow, pipeline);
-        GlShader geomShader = createGeometryShader(device, fogMode, pass, shadow, pipeline);
-        GlShader fragShader = createFragmentShader(device, fogMode, pass, shadow, pipeline);
 
         try {
-            GlProgram.Builder builder = GlProgram.builder(new Identifier("sodium", "chunk_shader_for_" + pass.toString().toLowerCase(Locale.ROOT) + (shadow ? "_gbuffer" : "_shadow")));
-
-            if (geomShader != null) {
-                builder.attachShader(geomShader);
-            }
-
-            return builder.attachShader(vertShader)
+            return GlProgram.builder(new Identifier("sodium", "chunk_shader"))
+                    .attachShader(vertShader)
                     .attachShader(fragShader)
                     .bindAttribute("a_Pos", ChunkShaderBindingPoints.POSITION)
                     .bindAttribute("a_Color", ChunkShaderBindingPoints.COLOR)
                     .bindAttribute("a_TexCoord", ChunkShaderBindingPoints.TEX_COORD)
                     .bindAttribute("a_LightCoord", ChunkShaderBindingPoints.LIGHT_COORD)
-                    .bindAttribute("mc_Entity", IrisChunkShaderBindingPoints.BLOCK_ID)
-                    .bindAttribute("mc_midTexCoord", IrisChunkShaderBindingPoints.MID_TEX_COORD)
-                    .bindAttribute("at_tangent", IrisChunkShaderBindingPoints.TANGENT)
-                    .bindAttribute("a_Normal", IrisChunkShaderBindingPoints.NORMAL)
                     .bindAttribute("d_ModelOffset", ChunkShaderBindingPoints.MODEL_OFFSET)
-                    .build((program, name) -> {
-                        if (pipeline != null) {
-                            ProgramUniforms uniforms = pipeline.initUniforms(name);
-                            ProgramSamplers samplers;
-
-                            if (shadow) {
-                                samplers = pipeline.initShadowSamplers(name);
-                            } else {
-                                samplers = pipeline.initTerrainSamplers(name);
-                            }
-
-                            return new IrisChunkProgram(device, program, name, uniforms, samplers);
-                        } else {
-                            return new ChunkProgram(device, program, name, fogMode.getFactory());
-                        }
-                    });
+                    .build((program, name) -> new ChunkProgram(device, program, name, fogMode.getFactory()));
         } finally {
             vertShader.delete();
-            if (geomShader != null) {
-                geomShader.delete();
-            }
             fragShader.delete();
         }
     }
 
     @Override
     public final void createShaders(RenderDevice device) {
-        WorldRenderingPipeline worldRenderingPipeline = Iris.getPipelineManager().getPipeline();
-        SodiumTerrainPipeline sodiumTerrainPipeline = null;
-
-        if (worldRenderingPipeline != null) {
-            sodiumTerrainPipeline = worldRenderingPipeline.getSodiumTerrainPipeline();
-        }
-
-        Iris.getPipelineManager().clearSodiumShaderReloadNeeded();
-
-        for (ChunkFogMode fogMode : ChunkFogMode.values()) {
-            this.programs.put(fogMode, createShadersForFogMode(device, fogMode, false, sodiumTerrainPipeline));
-            this.shadowPrograms.put(fogMode, createShadersForFogMode(device, fogMode, true, sodiumTerrainPipeline));
-        }
-    }
-
-    private EnumMap<BlockRenderPass, ChunkProgram> createShadersForFogMode(RenderDevice device, ChunkFogMode mode, boolean shadow,
-                                                                           SodiumTerrainPipeline pipeline) {
-        EnumMap<BlockRenderPass, ChunkProgram> shaders = new EnumMap<>(BlockRenderPass.class);
-
-        for (BlockRenderPass pass : BlockRenderPass.VALUES) {
-            shaders.put(pass, this.createShader(device, mode, pass, this.vertexFormat, shadow, pipeline));
-        }
-
-        return shaders;
+        // Iris start
+        this.device = device;
+        irisChunkProgramOverrides.createShaders(device);
+        // Iris end
+        this.programs.put(ChunkFogMode.NONE, this.createShader(device, ChunkFogMode.NONE, this.vertexFormat));
+        this.programs.put(ChunkFogMode.LINEAR, this.createShader(device, ChunkFogMode.LINEAR, this.vertexFormat));
+        this.programs.put(ChunkFogMode.EXP2, this.createShader(device, ChunkFogMode.EXP2, this.vertexFormat));
     }
 
     @Override
@@ -186,50 +79,43 @@ public abstract class ChunkRenderShaderBackend<T extends ChunkGraphicsState>
 
     @Override
     public void begin(MatrixStack matrixStack, BlockRenderPass pass) {
-        if (Iris.getPipelineManager().isSodiumShaderReloadNeeded()) {
-            RenderDevice device = this.programs.get(ChunkFogMode.LINEAR).get(BlockRenderPass.SOLID).getDevice();
-            deleteShaders();
-            createShaders(device);
+        this.activeProgram = this.programs.get(LegacyFogHelper.getFogMode());
+        // Iris start
+        {
+            if (ShadowRenderingState.areShadowsCurrentlyBeingRendered()) {
+                // No back face culling during the shadow pass
+                // TODO: Hopefully this won't be necessary in the future...
+                RenderSystem.disableCull();
+            }
+
+            ChunkProgram override = irisChunkProgramOverrides.getProgramOverride(device, pass);
+
+            if (override != null) {
+                this.activeProgram = override;
+            }
         }
-
-        if (ShadowRenderingState.areShadowsCurrentlyBeingRendered()) {
-            this.activeProgram = this.shadowPrograms.get(LegacyFogHelper.getFogMode()).get(pass);
-
-            // No back face culling during the shadow pass
-            // TODO: Hopefully this won't be necessary in the future...
-            RenderSystem.disableCull();
-        } else {
-            this.activeProgram = this.programs.get(LegacyFogHelper.getFogMode()).get(pass);
-        }
-
+        // Iris end
         this.activeProgram.bind();
         this.activeProgram.setup(matrixStack, this.vertexType.getModelScale(), this.vertexType.getTextureScale());
-    }
-
-    private void deleteShaders() {
-        for (EnumMap<BlockRenderPass, ChunkProgram> shaders: this.programs.values()) {
-            for (ChunkProgram shader : shaders.values()) {
-                shader.delete();
-            }
-        }
-
-        for (EnumMap<BlockRenderPass, ChunkProgram> shaders: this.shadowPrograms.values()) {
-            for (ChunkProgram shader : shaders.values()) {
-                shader.delete();
-            }
-        }
     }
 
     @Override
     public void end(MatrixStack matrixStack) {
         this.activeProgram.unbind();
         this.activeProgram = null;
+        // Iris start
         ProgramUniforms.clearActiveUniforms();
+        // Iris end
     }
 
     @Override
     public void delete() {
-        deleteShaders();
+        // Iris start
+        irisChunkProgramOverrides.deleteShaders();
+        // Iris end
+        for (ChunkProgram shader : this.programs.values()) {
+            shader.delete();
+        }
     }
 
     @Override
